46869984244b4caeca1c02d75a1619cb2e6e3e50
dondyprax <juan.acosta7a@gmail.com>
2018-09-21 02:49:58 -0600

    Primer commit

---
+++ Ejercicio_7.js
@@ -0,0 +1,9 @@
+
+function bisiesto(year = prompt("Ingrese un anio: ")) {
+    if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+        console.log(year + ' es bisiesto');
+        return;
+    }
+    console.log(year + ' no es bisiesto');
+    return;
+}
